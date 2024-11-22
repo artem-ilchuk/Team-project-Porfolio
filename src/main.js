5d92fac8e6286dfbda98c041ae493299836056be
artem-ilchuk <pmumart@gmail.com>
2024-11-22 02:53:57 +0200

    initialization

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/faq.js';
+import './js/mob_menu.js';
+import './js/my_projects.js';
+import './js/swiper-api';
+import './js/theme_switcher.js';
+import './js/work_together.js';
+import './js/reviews';
