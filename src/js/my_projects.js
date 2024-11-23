fa9b16e14cecd7188524c9bb9fea452808986b21
Denys D <ouctek786@gmail.com>
2024-11-23 13:32:59 +0200

    my_project_btn

--- src/js/my_projects.js
+++ src/js/my_projects.js
@@ -9,21 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
   loadMoreButton.addEventListener("click", () => {
     const hiddenProjects = document.querySelectorAll(".list_my_project.hidden");
+    const remainingProjects = hiddenProjects.length;
+
     let shown = 0;
 
     hiddenProjects.forEach((project) => {
-      if (shown < projectsPerClick && project.classList.contains("hidden")) {
-        project.style.display = "block"; 
+      if (shown < projectsPerClick) {
+        project.style.display = "block";
         setTimeout(() => {
           project.classList.remove("hidden");
-          project.classList.add("show"); 
-        }, 100); 
+          project.classList.add("show");
+        }, 100);
         shown++;
       }
     });
 
-    if (document.querySelectorAll(".list_my_project.hidden").length === 0) {
-      loadMoreButton.style.display = "none";
+ 
+    if (remainingProjects <= projectsPerClick) {
+      setTimeout(() => {
+        loadMoreButton.style.display = "none";
+      }, 100); 
     }
   });
 });
