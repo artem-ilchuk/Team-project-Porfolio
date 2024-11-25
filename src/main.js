b83a3467f5ef22add981a9fbbc7d999fac6bf060
artem-ilchuk <pmumart@gmail.com>
2024-11-25 20:01:05 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './js/faq.js';
 import './js/mob_menu.js';
 import './js/my_projects.js';
-import './js/swiper-api';
 import './js/theme_switcher.js';
 import './js/work_together.js';
 import './js/reviews';
