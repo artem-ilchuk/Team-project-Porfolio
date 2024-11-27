29814e24d1795bc2ee0af5f8c1d687d20c30009a
artem-ilchuk <pmumart@gmail.com>
2024-11-27 16:58:43 +0200

    update

--- src/main.js
+++ src/main.js
@@ -4,3 +4,21 @@ import './js/my_projects.js';
 import './js/theme_switcher.js';
 import './js/work_together.js';
 import './js/reviews';
+
+let hellopreloader = document.querySelector('.preloader');
+function fadeOutnojquery(el) {
+  el.style.opacity = 1;
+  let interhellopreloader = setInterval(function () {
+    el.style.opacity = el.style.opacity - 0.05;
+    if (el.style.opacity <= 0.05) {
+      clearInterval(interhellopreloader);
+      hellopreloader.style.display = 'none';
+    }
+  }, 20);
+}
+
+window.onload = function () {
+  setTimeout(function () {
+    fadeOutnojquery(hellopreloader);
+  }, 2500);
+};
